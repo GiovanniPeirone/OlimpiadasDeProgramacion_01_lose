53034b8bcd8f0033ff34031ab066ef3fed631a22
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-08-28 20:38:28 -0300

    a

---
+++ actividad_01/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    div_t resultado;
+
+
+    const int NumeroDeResiduos = 50; //NumeroDeResiduos
+    int base = 2; //BASE
+    int numero = 1000000000; //NUMERO
+
+    int Residuos[NumeroDeResiduos] = {};
+
+
+    for(int i = 0 ; NumeroDeResiduos > i; i++ )
+    {
+
+        resultado = div(numero,base);
+        Residuos[i] = resultado.rem;
+
+        numero = resultado.quot;
+
+        cout << Residuos[i];
+
+    }
+
+    return 0;
+}
