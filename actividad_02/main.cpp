bddd2c67b5454a0ae197bb5dfd10ab98e82c2f1f
Giovanni <134148500+GiovanniPeirone@users.noreply.github.com>
2024-09-09 12:54:01 -0300

    a

--- actividad_02/main.cpp
+++ actividad_02/main.cpp
@@ -10,6 +10,7 @@ int main()
 
     //mapear letras en el array
     int x = 0;
+    cout << "--Lentras--";
     for (int i = 0; 26 > i; i++)
     {
         Letras[i][x] = Letra;
@@ -20,22 +21,25 @@ int main()
         cout << Letras[i][x+1];
     }
 
-
-
-
     string texto = "ashdgaKASDJGHAKSJ_333sdjkhasSDALKJH";
+    string NewTexto;
 
-
-    /*
-    for (int i = 0 ; text.length() > i ; i++)
+    cout << texto;
+    for (int i = 0 ; texto.length() > i ; i++)
     {
-        if (text.length(i) =< 90)
+        if (texto.lenght(i) < 91)
         {
-
+            for (int a = 0 ; Letras.zize() > a; a++)
+            {
+                if (texto.lenght(i) == Letras[a][0])
+                {
+                    NewTexto += Letras[a][1]
+                }
+            }
         }
     }
 
-    */
+
 
 
     return 0;
