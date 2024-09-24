4e9a9681e4a47703b4fb5a46e8825c274d08e3b9
Giovanni <134148500+GiovanniPeirone@users.noreply.github.com>
2024-09-24 13:19:19 -0300

    asd

--- Actividad_03/main.cpp
+++ Actividad_03/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int panes(const int ciudades,const int peajes)
 {
+    int PanesEnCiudades[ciudades] = {3, 1}
+    int PanesPeajes[peajes] = {1}
+
+    for (int i = 0 ; ciudades > i; i++){
+
+    }
 
     return 0;
 }
